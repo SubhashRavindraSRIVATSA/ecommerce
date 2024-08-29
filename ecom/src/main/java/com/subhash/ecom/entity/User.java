8d0f74349009fe062f474d86f8a21666285f95ea
SubhashRavindraSRIVATSA <72192507+SubhashRavindraSRIVATSA@users.noreply.github.com>
2024-08-29 23:58:59 +0200

    ecomm-server:created User entity and UserRole enum

---
+++ ecom/src/main/java/com/subhash/ecom/entity/User.java
@@ -0,0 +1,27 @@
+package com.subhash.ecom.entity;
+
+import com.subhash.ecom.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private UserRole role;
+
+    @Lob
+    @Column(columnDefinition = "longBlob")
+    private byte[] img;
+}
---
+++ ecom/src/main/java/com/subhash/ecom/enums/UserRole.java
@@ -0,0 +1,7 @@
+package com.subhash.ecom.enums;
+
+public enum UserRole {
+
+    ADMIN,
+    CUSTOMER
+}
